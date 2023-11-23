06da7a15e672c99487c746ec321f29bb0f54e3e8
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2023-11-23 14:55:03 +0100

    got their heads

--- fetchteiheader.py
+++ fetchteiheader.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env python3
-import os
-from lxml  import etree
-#from lxml.etree import parse
+import glob
+from lxml import etree
+from urllib.request import urlopen
 from datetime import datetime
-import requests
 
-nsmap = {None: 'http://www.tei-c.org/ns/1.0',
-         'wib': 'https://wibarab.acdh.oeaw.ac.at/langDesc',
-         'xi': 'http://www.w3.org/2001/XInclude',
-         'xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-         'tei': 'http://www.tei-c.org/ns/1.0',
-         'mets': 'http://www.loc.gov/METS/v2',
-         'mod': 'http://www.loc.gov/mods/v3',
-         'xml': ''
-         }
-
-
-#parser = etree.XMLParser(recover=True)
 source_directory = 'teitest'
 logn = 'fetchteiheader'
+
+
+nspaces = {
+    "tei": "http://www.tei-c.org/ns/1.0",
+    "xml": "http://www.w3.org/XML/1998/namespace",
+    "mets": "http://www.loc.gov/METS/"}
 
 
 def log(logname, filename, arg='could not be parsed'):
@@ -28,42 +21,24 @@ def log(logname, filename, arg='could not be parsed'):
                 f'File `{filename}` {arg}\n')
 
 
-def checkremote(filename):
-    root = content = False
-    try:
-        content = getxml(filename)
-    except Exception:
-        log(logn, filename, 'could not be retrieved')
-    if content:
-        tree = etree.fromstring(content)
-        # root = tree.getroot()
-#        try:
-            #    tree = ElementTree.fromstring(content)
- #           print(tree)
-            # tree = parse(response, parser)
-  #          print('AAAAA')
- #           root = tree.getroot()
-  #      except Exception:
-   #         log(logn, filename)
-    return tree
-
-
-def getheader(root):
-    # stub
-    return root.xpath('.//mets:dmdSec', namespaces=nsmap)
+def getheader(tree):
+    header = tree.xpath('//mets:dmdSec', namespaces=nspaces)
+    return header[0]
 
 
 def getxml(docid):
+    tree = False
     url = f'https://viewer.acdh.oeaw.ac.at/viewer/sourcefile?id={docid}'
-    response = requests.get(url)
-    return response.content
+    parser = etree.XMLParser()
+    with urlopen(url) as u:
+        tree = etree.parse(u, parser)
+        root = tree.getroot()
+    return root
 
 
-directorycontents = os.listdir(source_directory)
-
-for filename in directorycontents:
+for filename in glob.glob(f'{source_directory}/*.xml'):
     filename = 'A63_51.xml'
-    if xmlmets := checkremote(filename.rstrip('.xml')):
+    if xmlmets := getxml(filename.rstrip('.xml')):
         header = getheader(xmlmets)
-        print(header)
+        print(header.tostring())
         break  # print(xmlmets)
