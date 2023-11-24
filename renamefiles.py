78647794b8a37cb01bd7eb133d30967a1c723fb4
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2023-11-24 09:36:15 +0100

    gloob, os.path.join and acdh_tei_pyutils. Flake8 compliant and Black formatted

--- renamefiles.py
+++ renamefiles.py
@@ -1,56 +1,43 @@
 #!/usr/bin/env python3
+import glob
 import os
-from lxml import etree
-from lxml.etree import parse
 from datetime import datetime
+from acdh_tei_pyutils.tei import TeiReader
 
-nsmap = {None: 'http://www.tei-c.org/ns/1.0',
-         'wib': 'https://wibarab.acdh.oeaw.ac.at/langDesc',
-         'xi': 'http://www.w3.org/2001/XInclude',
-         'xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-         'tei': 'http://www.tei-c.org/ns/1.0',
-         'xml': ''
-         }
-parser = etree.XMLParser(recover=True)
-directory = 'tei'
-logn = 'renamefiles'
+directory = "tei"
+logn = "renamefiles"
 
 
-def log(logname, filename, arg='could not be parsed'):
+# The first mandatory argument is the name of the log file, the second one is the file mentioned in the log entry.
+def log(logname, filename, arg="could not be parsed"):
     now = datetime.now()
-    with open(f'{logname}.log', 'a') as f:
-        f.write(f'{now.strftime("%Y/%m/%d %H:%M:%S")}\t'
-                f'File `{filename}` {arg}\n')
+    with open(f"{logname}.log", "a") as f:
+        f.write(f'{now.strftime("%Y/%m/%d %H:%M:%S")}\tFile `{filename}` {arg}\n')
 
 
 def checkfile(filename):
-    root = False
-    if os.path.exists(filename):
-        try:
-            tree = parse(filename, parser)
-            root = tree.getroot()
-        except Exception:
-            log(logn, filename)
-    else:
-        log(logn, filename, 'does not exist')
-    return root
+    tree = False
+    try:
+        tree = TeiReader(filename)
+    except Exception:
+        log(logn, filename)
+    return tree
 
 
 def getname(root):
     name = False
-    docids = root.findall('.//tei:title[@type="main"]', namespaces=nsmap)
+    docids = root.any_xpath('//tei:teiHeader//tei:title[@type="main"]')
     for i in docids:
-        parts = i.text.split('_')
+        parts = i.text.split("_")
+        # Not being sure of whether the content of each element <title type="main"> is what we are looking for, we get
+        # the first one that follows the format we're expecting to find to improve our chances
         if len(parts) > 2:
-            name = f'{parts[1]}_{parts[2]}'
+            name = f"{parts[1]}_{parts[2]}"
             break
     return name
 
 
-directorycontents = os.listdir(directory)
-
-for filename in directorycontents:
-    old_name = f'{directory}/{filename}'
-    if xmltei := checkfile(old_name):
-        new_name = f'{directory}/{getname(xmltei)}.xml'
-        os.rename(old_name, new_name)
+for filename in glob.glob(os.path.join(directory, "*.xml")):
+    if xmltei := checkfile(filename):
+        new_name = os.path.join(directory, f"{getname(xmltei)}.xml")
+        os.rename(filename, new_name)
