cc113dcfa4dc008617ba535c73522885340a264e
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2023-11-27 11:40:53 +0100

    dates normalised according to TEI guidelines

--- fetchteiheader.py
+++ fetchteiheader.py
@@ -5,6 +5,7 @@ from urllib.request import urlopen
 from datetime import datetime
 import json
 import os
+import re
 
 source_directory = 'teitest'
 logn = 'fetchteiheader'
@@ -30,14 +31,51 @@ def getxml(docid):
     root = False
     url = f'https://viewer.acdh.oeaw.ac.at/viewer/sourcefile?id={docid}'
     try:
-        print(url)
         with urlopen(url) as u:
             tree = etree.parse(u, parser)
             root = tree.getroot()
     except Exception:
         log(logn, url)
-    print('RROT', root)
     return root
+
+
+def parseattributes(element, value):
+    if 'date' in element.tag:
+        attr = normalisedate(value)
+        for x in attr:
+            element.attrib[x] = attr[x]
+    element.text = value
+    return element
+
+
+def normalisedate(date):
+    try:
+        year = int(date.lstrip('~').replace('x', '0').split()[0].split('-')[0].strip('.'))
+    except Exception:
+        log(logn, date, 'is not a valid date')
+        year = 'nan'
+    ddate = {'when': f'{year}'}
+    if date.startswith('~'):
+        ddate = {'notBefore': f'{year - 20}', 'notAfter': f'{year + 20}'}
+    elif date.endswith('Jh.'):
+        ddate = {'notBefore': f'{(year - 1) * 100}', 'notAfter':  f'{(year - 1) * 100 + 99}'}
+    elif date.endswith('xx'):
+        ddate = {'notBefore': f'{year}', 'notAfter': '{year + 99}'}
+    elif re.findall(r'^\d{2}\-\d(?:/\d)*', date):
+        ddate = {'notBefore': f'{year * 100}', 'notAfter': '{year + 99}'}
+        second = date.split('-')[1]
+        if second == '1':
+            nb = year * 100
+            na = nb + 50
+        elif second == '2':
+            na = year * 100 + 100
+            nb = na - 50
+        else:
+            factor = int(second.split('/')[0]) / int(second.split('/')[1])
+            na = year * 100 + int(factor * 100)
+            nb = year * 100 + int(factor * 100) - 25
+        ddate = {'notBefore': f'{nb}', 'notAfter': f'{na}'}
+    return ddate
 
 
 def printtree(tree):
@@ -61,15 +99,13 @@ def add_nodes(teitree, nodes, value):
     if len(nodes) > 1:
         add_nodes(parent, nodes[1:], value)
     else:
-        parent.text = value
+        parent = parseattributes(parent, value)
     return teitree
 
 
 for filename in glob.glob(os.path.join(source_directory, '*.xml')):
+    log(logn, filename, 'Parsing')
     # filename = 'A63_51.xml'
-    print(filename)
     if xmlmets := getxml(os.path.basename(filename).rstrip('.xml')):
-        print(xmlmets)
         a = mets2tei(filename, xmlmets, dictionary)
         printtree(a)
-        break   # For testing purposes
