12919057d3431e020297b147996ae18f999173b9
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-01-05 13:33:45 +0100

    can rename but invalid XML pass through

--- renamefiles.py
+++ renamefiles.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import glob
 import os
-from acdh_tei_pyutils.tei import TeiReader
+from lxml import etree as ET
 from maketei import Log
 import re
 
@@ -10,18 +10,23 @@ log = Log("0rename")
 
 
 def checkfile(filename):
+    parser = ET.XMLParser(recover=True)
     tree = False
     try:
-        tree = TeiReader(filename)
-    except Exception as e:
-        log.print_log(filename, e, True)
-    return tree
+        tree = ET.parse(filename)
+    except Exception:
+        try:
+            tree = ET.parse(filename, parser=parser)
+        except Exception as e:
+            log.print_log(filename, e, True)
+    return tree.getroot()
 
 
 def getname(root):
     name = False
     regex = re.compile(r'A-Gf_(\w)_*(\d{1,2}_\S{2,3})_.*')
-    docid = root.any_xpath('//tei:teiHeader//tei:title[@type="main"]')[0].text
+    docid = root.xpath('//tei:teiHeader//tei:title[@type="main"]',
+                       namespaces={"tei": "http://www.tei-c.org/ns/1.0"})[0].text
     name = re.sub(regex, r'\g<1>\g<2>', docid)
     return name
 
