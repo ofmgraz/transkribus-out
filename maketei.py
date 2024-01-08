e001d422624d54fddc5a2ccb2c155d7707b2276a
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-01-08 17:18:13 +0100

    runs. Let's check out the code

--- maketei.py
+++ maketei.py
@@ -141,10 +141,7 @@ class TeiTree:
                 cert = False
             origins[0] = origins[0].strip("? ")
             dictentry = locdict[origins[0]]
-            place.attrib[
-                "{http://www.w3.org/XML/1998/namespace}id"
-            ] = dictentry['attr']['id']
-                    place.attrib["ref"] = dictentry['attr']['ref']
+            place = ET.SubElement(tree, "place", attrib={"{http://www.w3.org/XML/1998/namespace}id": dictentry['id']})
             ET.SubElement(place, "placeName").text = origins[0]
             location = ET.SubElement(place, "location")
             ET.tostring(location, pretty_print=True, encoding="unicode")
@@ -152,15 +149,14 @@ class TeiTree:
                 if data == "idno":
                     atb = {"type": "Wikidata"}
                 else:
-                    atb == {}
+                    atb = {}
                 ET.SubElement(location, i, attrib=atb).text = dictentry["data"]["location"][i]
             # Element in msDesc/history referencing the entry above
             provenance = self.msdesc.xpath(
                 "./tei:history/tei:provenance", namespaces=nsmap
             )[0]
-            placename = ET.SubElement(provenance, "placeName")
+            placename = ET.SubElement(provenance, "placeName", attrib={"ref": f"#{dictentry['id']}"})
             placename.text = origins[0]
-            placename.attrib["ref"] = f"#{dictentry['attr']['id']}"
             if cert:
                 placename.attrib["cert"] = "medium"
             if publisher:
@@ -171,7 +167,7 @@ class TeiTree:
                     publisher,
                 )
         if len(origins) > 1 and origins[1].strip("? ") in locdict:
-            self.parse_origin(",".join(origins[1:]))
+            self.parse_origin(",".join(origins[1:]), False)
 
     def make_publisher(self, place, publisher):
         tree = ET.SubElement(self.tei.any_xpath(".//tei:standOff")[0], "listPerson")
@@ -184,16 +180,13 @@ class TeiTree:
             if data == "idno":
                 atb = {"type": "GND"}
             else:
-                atb == {}
-            ET.SubElement(person, data, attrib=atb).text = dictentry["data"][att]
+                atb = {}
+            ET.SubElement(person, data, attrib=atb).text = dictentry["data"][data]
         bibl = self.header.xpath(
             "//tei:fileDesc/tei:sourceDesc/tei:bibl", namespaces=nsmap
         )[0]
         ET.SubElement(bibl, "pubPlace").append(place)
-        pub = ET.SubElement(bibl, "publisher")
-        pub.text = publisher.strip()
-
-        pub.attrib["ref"] = f"#{publisher.lower()}"
+        ET.SubElement(bibl, "publisher", attrib={"ref": f"#{publisher.lower()}"}).text = publisher.strip()
         self.msdesc.xpath("//tei:physDesc/tei:objectDesc", namespaces=nsmap)[0].attrib[
             "form"
         ] = "print"
@@ -245,23 +238,19 @@ class TeiTree:
         )
         if lit:
             keys = f"#{lit.lower()}"
-            cat = ET.Element("category")
-            cat.attrib["{http://www.w3.org/XML/1998/namespace}id"] = lit.lower()
+            cat = ET.Element("category", attrib={"{http://www.w3.org/XML/1998/namespace}id": lit.lower()})
             ET.SubElement(cat, "catDesc").text = lit
             taxonomies[1].append(cat)
         else:
             keys = ""
         books = " ".join(" ".join(booktype.split(",")).split("/")).split()
         for book in books:
-            for booktype in bookdict:
+            for btype in bookdict:
                 if booktype in book.lower() and bookdict[booktype] not in keys:
-                    cat = ET.Element("category")
-                    cat.attrib["{http://www.w3.org/XML/1998/namespace}id"] = bookdict[
-                        booktype
-                    ]
+                    cat = ET.Element("category", attrib={"{http://www.w3.org/XML/1998/namespace}id": bookdict[btype]})
                     ET.SubElement(cat, "catDesc").text = book
                     taxonomies[0].append(cat)
-                    keys += f" #{bookdict[booktype]}"
+                    keys += f" #{bookdict[btype]}"
         return keys
 
     def parse_summary(self, summary, bookt, attributes, line):
