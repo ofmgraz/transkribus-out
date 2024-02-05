f2d9696bb17cf63c521bc162d4f43dbfc8982368
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-02-05 13:35:01 +0100

    deals with multiple path possibilities

--- scripts/getchecksum.py
+++ scripts/getchecksum.py
@@ -6,7 +6,8 @@ import sys
 
 now = datetime.now().strftime("%Y-%m-%dT%H:%M")
 output_file = "checksum.csv"
-modus = "w"
+file_list = []
+subdirs = []
 
 
 def get_hash(input_file):
@@ -17,18 +18,27 @@ def get_hash(input_file):
 
 
 def parse_dir(path, file_list=[]):
-    print(f'Reading directory "{path}"')
-    for dirpath, dirnames, filenames in os.walk(path):
-        for f in filenames:
-            fullpath = os.path.join(dirpath, f)
-            file_list.append(f'"{f}",{get_hash(fullpath)},{now}\n')
-            print(f"[OK]\t{fullpath}")
-        [parse_dir(os.path.join(dirpath, d), file_list) for d in dirnames]
+    print(f' ...Reading directory "{path}"')
+    if os.path.isfile(path):
+        file_list.append(f'"{path}",{get_hash(path)},{now}\n')
+        print(f"[OK]\t{path}")
+    else:
+        for dirpath, dirnames, filenames in os.walk(path):
+            for f in filenames:
+                fullpath = os.path.join(dirpath, f)
+                file_list.append(f'"{f}",{get_hash(fullpath)},{now}\n')
+                print(f"[OK]\t{fullpath}")
+            [parse_dir(os.path.join(dirpath, d), file_list) for d in dirnames]
     return file_list
 
 
 for folder in sys.argv[1:]:
-    file_list = parse_dir(folder)
-    with open(output_file, modus) as f:
-        f.writelines(file_list)
-        modus = "a"
+    if os.path.isfile(folder):
+        file_list.append(f'"{folder}",{get_hash(folder)},{now}\n')
+        print(f"[OK]\t{folder}")
+    else:
+        subdirs.append(folder)
+for folder in subdirs:
+    file_list = parse_dir(folder, file_list)
+with open(output_file, "w") as f:
+    f.writelines(file_list)
