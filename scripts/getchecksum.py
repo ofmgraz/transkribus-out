62cba2d17d0489a01f6a8c3c81363508bfc26b5c
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-02-22 08:20:11 +0100

    multiple-checks bug solved

--- scripts/getchecksum.py
+++ scripts/getchecksum.py
@@ -8,37 +8,31 @@ now = datetime.now().strftime("%Y-%m-%dT%H:%M")
 output_file = "checksum.csv"
 file_list = []
 subdirs = []
+with open(output_file, "w") as out_file:
+    out_file.write(f'FILENAME,{now}')
 
 
 def get_hash(input_file):
-    with open(input_file, "rb") as f:
-        file_contents = f.read()
+    with open(input_file, "rb") as in_file:
+        file_contents = in_file.read()
         md5_hash = hashlib.md5(file_contents).hexdigest()
     return md5_hash
 
 
-def parse_dir(path, file_list=[]):
-    print(f' ...Reading directory "{path}"')
+def parse_dir(path):
+    base_name = os.path.basename(path)
     if os.path.isfile(path):
-        file_list.append(f'"{path}",{get_hash(path)},{now}\n')
-        print(f"[OK]\t{path}")
+        md5 = get_hash(path)
+        with open(output_file, "a") as out_file:
+            out_file.write(f'{base_name},{md5}\n')
+        print(f"[OK]\t{base_name}:\t{md5}")
+    elif os.path.isdir(path):
+        print(f"Entering in {path}")
+        for subfile in os.listdir(path):
+            parse_dir(os.path.join(path, subfile))
     else:
-        for dirpath, dirnames, filenames in os.walk(path):
-            for f in filenames:
-                fullpath = os.path.join(dirpath, f)
-                file_list.append(f'"{f}",{get_hash(fullpath)},{now}\n')
-                print(f"[OK]\t{fullpath}")
-            [parse_dir(os.path.join(dirpath, d), file_list) for d in dirnames]
-    return file_list
+        print(f"[KO]\t{path}: Filetype unknown.")
 
 
 for folder in sys.argv[1:]:
-    if os.path.isfile(folder):
-        file_list.append(f'"{folder}",{get_hash(folder)},{now}\n')
-        print(f"[OK]\t{folder}")
-    else:
-        subdirs.append(folder)
-for folder in subdirs:
-    file_list = parse_dir(folder, file_list)
-with open(output_file, "w") as f:
-    f.writelines(file_list)
+    parse_dir(folder)
