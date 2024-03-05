0148a20a84376e928709c5c32124f7ca4873709f
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-03-05 13:03:36 +0100

    simpler

--- scripts/getchecksum.sh
+++ scripts/getchecksum.sh
@@ -7,29 +7,19 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RED='\033[0;31m'
 REG='\033[0m'
-DATE=`date -Im`
+DATE=`date +"%Y%m%d%H%M%S"`
 OUTPUTFILE="checksums.csv"
 
 # If first argument is -d, it does not overwrite the log file.
 # Needed for recursive calls to explore subdirectories
-if [ "$1" != "-d" ]; then
-	echo > $OUTPUTFILE
-else
-	for ARG do
-		shift
-		[ ${ARG} = "-d" ] && continue
-		set -- "$@" "${ARG}"
-	done
-fi
-
-for FILE in $@; do
+for FILE in "$@"; do
 	# If it is a directory, the script calls itself
-	if [ -d ${FILE} ]; then
+	if [ -d "$FILE" ]; then
 		echo "[${YELLOW}--${REG}]\tEntering directory ${FILE}"
-		eval "./$0 -d ${FILE}/*"
+		eval "./$0  \"$FILE\"/*"
 	# If it is a regular file, it gets the checksum
 	elif [ -f "$FILE" ]; then
-		echo "`cksum --untagged -a md5 ${FILE}|awk '{print $2","$1}'`,${DATE}" |tee -a ${OUTPUTFILE}
+		echo "`cksum --untagged -a md5 \"$FILE\"|awk '{print $2","$1}'`" |tee -a $OUTPUTFILE
 		echo "[${GREEN}OK${REG}]\t${FILE}"
 	# In case it is something else such as symlink or a device, the script ignores it
 	else
