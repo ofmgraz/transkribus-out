3f79fcc274b2b5f6578065b39d2313defeaee502
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-03-06 10:20:12 +0100

    trnd

---
+++ transform.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+import glob
+from saxonche import PySaxonProcessor
+
+XSLT = "https://csae8092.github.io/page2tei/page2tei-0.xsl"
+METS_DIR = "./mets"
+TEI_DIR = "./data/editions"
+os.makedirs(TEI_DIR, exist_ok=True)
+col_id = "216937"
+
+files = glob.glob(f"{METS_DIR}/{col_id}/*_mets.xml")
+for x in files:
+    tail = os.path.split(x)[-1]
+    doc_id = tail.split("_")[0]
+    tei_file = f"{doc_id}.xml"
+    print(f"transforming mets: {x} to {tei_file}")
+    with PySaxonProcessor(license=False) as proc:
+        xsltproc = proc.new_xslt30_processor()
+        xsltproc.set_parameter("combine", proc.make_boolean_value(True))
+        xsltproc.set_parameter("ab", proc.make_boolean_value(True))
+        document = proc.parse_xml(xml_file_name=x)
+        executable = xsltproc.compile_stylesheet(stylesheet_file=XSLT)
+        output = executable.transform_to_string(xdm_node=document)
+        output = output.replace(' type=""', "")
+        with open(os.path.join(TEI_DIR, tei_file), "w") as f:
+            f.write(output)
