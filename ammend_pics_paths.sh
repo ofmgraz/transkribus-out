b58f00312848dd1160a3cc4482d73c93bc31f9be
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-05-08 14:10:40 +0200

    ammend paths

--- ammend_pics_paths.sh
+++ ammend_pics_paths.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-sed -i 's/A-Gf_S1_76/A-Gf_S1_73/g' data/editions/1527232.xml 
-sed -i 's/A-Gf_S1_65/A-Gf_S1_69/g' data/editions/1529931.xml 
-
+sed -i 's/A\-Gf_S1_65/A-Gf_S1_69/g' data/editions/{S1_69,1529931}.xml 
+sed -i 's/A\-Gf_A64_41/A-Gf_64_41/g' data/editions/*.xml
