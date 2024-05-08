2fe4e5ce34d832a76c7f7baa62b373c277205214
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-05-08 17:16:21 +0200

    it works

--- ammend_duplicate.py
+++ ammend_duplicate.py
@@ -4,16 +4,16 @@ import re
 # Solve duplicate xml:ids
 with open("data/editions/1499840.xml") as f:
     text = f.read()
-xmlids = re.findall(r'xml:id=(\S+)', text)
-counter = 0
+xmlids = re.findall(r'xml:id="(\S+)"', text)
 xmlids.sort()
+counter = 0
 ant = ''
 for i in xmlids:
     if i == ant:
         counter += 1
     else:
         counter = 0
-    re.sub(f'"{i}"', f'"{i}_{counter}"', text, count=1)
+    text = re.sub(f'"({i})"', fr'"\1_{counter}"', text, count=1)
     ant = i
-with open("data/editions/1499840.xml", 'w') as file:
-    file.write(text)
+with open("data/editions/1499840.xml", "w") as f:
+    f.write(text)
