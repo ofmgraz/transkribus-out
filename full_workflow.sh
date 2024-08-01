2be1025c67c052631ad1aa36dbd506698eebd16e
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-08-01 09:07:40 +0200

    automating some stuff

--- full_workflow.sh
+++ full_workflow.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-cd data
+source ./secret.env
 rm -rf editions
-tar xvf numbers.tar
+./download.py
+rm -f data/editions/[A-Z]*
+cd data
+tar cvf numbers.tar editions
+# tar xvf numbers.tar
 cd ..
-./ammend_pics_paths.sh
+./amend_pics_paths.sh
 ./renamefiles.py
 ./generate_body.py
