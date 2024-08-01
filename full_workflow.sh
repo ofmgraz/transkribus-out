d32eecc87d8fd86e4699be42865560e5f4dab269
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-08-01 10:39:12 +0200

    renaming

--- full_workflow.sh
+++ full_workflow.sh
@@ -2,11 +2,13 @@
 . ./secret.env
 rm -rf editions
 ./download.py
+./transform.py
 rm -f data/editions/*[A-Z_]*
 cd data
 tar cvf numbers.tar editions
 # tar xvf numbers.tar
 cd ..
-./amend_pics_paths.sh
+./amend_pics_paths.sh || true
 ./renamefiles.py
+rm -f data/editions/*duplicated*
 ./generate_body.py
--- renamefiles.py
+++ renamefiles.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import glob
 import os
-from acdh_tei_pyutils.tei import TeiReader, ET
+from acdh_tei_pyutils.tei import ET
 from maketei import Log
 import re
 
@@ -27,12 +27,13 @@ def getname(root):
     regex = re.compile(r'A-Gf_(\w)_*(\d{1,2}_\S{2,3})_.*')
     docid = root.xpath('//tei:teiHeader//tei:title[@type="main"]',
                        namespaces={"tei": "http://www.tei-c.org/ns/1.0"})[0].text
-    name = re.sub(regex, r'\g<1>\g<2>', docid)
+    name = re.sub(regex, r"\g<1>\g<2>", docid)
+    if name[0] in '0123456789':
+        name = re.sub(r"^(\d+_)", r"A\g<1>", name)
     return name
 
 
-
-for  current_filepath in glob.glob(os.path.join(directory, "*.xml")):
+for current_filepath in glob.glob(os.path.join(directory, "*.xml")):
     try:
         xmltei = checkfile(current_filepath)
         current_file = f"{getname(xmltei)}.xml"
