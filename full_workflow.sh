33c346232606098bfcd386697e75f76429af96ec
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-08-06 10:50:09 +0200

    delete before transforming to save time

--- full_workflow.sh
+++ full_workflow.sh
@@ -2,13 +2,8 @@
 . ./secret.env
 rm -rf editions
 ./download.py
+rm -f mets/216937/{2926205,1523134,2914584,2926205,3374447,3374448,3374547,3376967,377127,3377287,3385787,3406471,3377127}* || true
 ./transform.py
-rm -f data/editions/*[A-Z_]*
-cd data
-tar cvf numbers.tar editions
-# tar xvf numbers.tar
-cd ..
 ./amend_pics_paths.sh || true
 ./renamefiles.py
-rm -f data/editions/*(duplicate|2926205|TRAINING|S1_44)*
 ./generate_body.py
