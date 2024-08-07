1af90539ca2829e8b703aeec1efa1dd66b4e9542
Fernando Sanz-Lázaro <54422069+fsanzl@users.noreply.github.com>
2024-08-07 13:17:47 +0200

    Update amend_pics_paths.sh

--- amend_pics_paths.sh
+++ amend_pics_paths.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sed -i 's/A\-Gf_S1_65/A-Gf_S1_69/g' data/editions/S1_69.xml data/editions/1529931.xml  || true
 sed -i 's/A\-Gf_A64_41/A-Gf_64_41/g' data/editions/*.xml || true 
-sed -i -r 's/(A\-Gf_S1_(2[356]|73?)-[[:digit:]]*)[rv]/\1/g' data/editions/*.xml ||true 
+sed -i -r 's/(A\-Gf_S1_(2[356]|73?)-[[:digit:]]*)[rv]/\1/g' data/editions/*.xml || true 
 sed -i -r 's/A-Gf_S1_65-001/A-Gf_S1_65-002/g' data/editions/*.xml || true 
 sed -i -r 's/S1_73\/files/S1_76\/files/g' data/editions/*.xml || true
 sed -i -r 's/\/64_41\/files/\/A64_41\/files/g' data/editions/*.xml ||true
