b128634b90d2dfd5819b27e1ea81b8437c9e6614
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-08-14 12:40:30 +0200

    path

--- add_declaration.sh
+++ add_declaration.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [[ -z $1 ]] ; then
-	XMLPATH="data/editions/*"
+	XMLPATH="data/editions"
 else
 	XMLPATH="$1"
 fi
 
-sed -i $'1s/^/<?xml version="1.0" encoding="utf-8"?>\\\n<?xml-model href="https:\\/\\/id.acdh.oeaw.ac.at\\/ofmgraz\\/schema.rng" type="application\\/xml" schematypens="http:\\/\\/relaxng.org\\/ns\\/structure\\/1.0"?>\\\n/' $XMLPATH
+sed -i $'1s/^/<?xml version="1.0" encoding="utf-8"?>\\\n<?xml-model href="https:\\/\\/id.acdh.oeaw.ac.at\\/ofmgraz\\/schema.rng" type="application\\/xml" schematypens="http:\\/\\/relaxng.org\\/ns\\/structure\\/1.0"?>\\\n/' $XMLPATH/*.xml
 
