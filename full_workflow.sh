a9d4effcbc0e88984a81181d5651324c0d7dd870
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-08-19 13:37:48 +0200

    matches error in zsh. Forcing bash

--- full_workflow.sh
+++ full_workflow.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 . ./secret.env
-rm -rf editions
+rm -rf mets/* data/editions/* tei_headers/*
 ./download.py
-rm -f mets/216937/{2914584,2926205,3374447,3374448,3374547,3376967,3377127,3377287,3385787,3406471,3674355}* || true
-./transform.py
+rm -f mets/*/{2914584,2926205,3374447,3374448,3374547,3376967,3377127,3377287,3385787,3406471,3674355}* ||true
+./transform.py |nl
 ./amend_duplicate.py || true
 ./amend_pics_paths.sh || true
 ./renamefiles.py
