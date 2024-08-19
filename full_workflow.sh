aa66c4636997d8ffd41f8eb05e472d3b5faefc2b
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-08-19 10:40:48 +0200

    amendments

--- full_workflow.sh
+++ full_workflow.sh
@@ -2,10 +2,11 @@
 . ./secret.env
 rm -rf editions
 ./download.py
-rm -f mets/216937/{2926205,1523134,2914584,2926205,3374447,3374448,3374547,3376967,377127,3377287,3385787,3406471,3377127}* || true
+rm -f mets/216937/{2914584,2926205,3374447,3374448,3374547,3376967,3377127,3377287,3385787,3406471,3674355}* || true
 ./transform.py
-./generate_headers.py
 ./amend_duplicate.py || true
 ./amend_pics_paths.sh || true
 ./renamefiles.py
+./generate_headers.py
 ./generate_body.py
+cp -f data/constants/* data/editions
