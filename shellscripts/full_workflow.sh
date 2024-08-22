8258307b5aaf9f18a3e6130b37c2f36a4086d378
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-08-22 10:34:12 +0200

    path

--- shellscripts/full_workflow.sh
+++ shellscripts/full_workflow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . ./secret.env
 rm -rf data/mets/* data/editions/* data/constants/tei_headers/*
-./download.py
+./pyscripts/download.py
 rm -f data/mets/*/{2914584,2926205,3374447,3374448,3374547,3376967,3377127,3377287,3385787,3406471,3674355}* ||true
 ./pyscripts/transform.py |nl
 ./pyscripts/amend_duplicate.py || true
