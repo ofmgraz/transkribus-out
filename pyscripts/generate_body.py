fda5787af4b72867a4b7f742b64cc477feac33dd
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-09-03 15:32:47 +0200

    Faster

--- pyscripts/generate_body.py
+++ pyscripts/generate_body.py
@@ -6,39 +6,61 @@ from os import path
 from sys import argv
 from acdh_tei_pyutils.tei import TeiReader, ET
 
+# Directory paths
 tkb_directory = "./data/editions"
 headers_directory = "./data/constants/tei_headers"
 schema_file = "tei_ms.xsd"
-i = 1
-test = False
-if len(argv) > 1:
-    test = True
 log = maketei.Log("0generate_body")
 
+# Check if running in test mode
+test = len(argv) > 1
+
+# Initialize previous file path
 prev_filepath = ""
 
-for tkb_file in glob.glob(path.join(tkb_directory, "*.xml")):
+# Cache TeiReader objects to minimize repeated file reads
+tei_cache = {}
+
+# Process each XML file in the directory
+for i, tkb_file in enumerate(glob.glob(path.join(tkb_directory, "*.xml")), start=1):
     print(f"{i}\t\tParsing {tkb_file}")
-    filename = tkb_file.split("/")[-1]
+    filename = path.basename(tkb_file)
     header_file = path.join(headers_directory, filename)
-    i += 1
-    if test:
-        tei_source = maketei.TeiBody(tkb_file, header_file)
-    else:
-        try:
+    
+    # Process the TEI body
+    try:
+        if test:
             tei_source = maketei.TeiBody(tkb_file, header_file)
-        except Exception as e:
-            error = f"{type(e).__name__} {__file__} {e.__traceback__.tb_lineno}"
-            log.print_log(filename, error, stdout=True)
+        else:
+            if filename not in tei_cache:
+                tei_cache[filename] = maketei.TeiBody(tkb_file, header_file)
+            tei_source = tei_cache[filename]
+    except Exception as e:
+        error = f"{type(e).__name__} {__file__} {e.__traceback__.tb_lineno}"
+        log.print_log(filename, error, stdout=True)
+        continue
+
     xml_current_root = tei_source.tei.tree.getroot()
+
+    # Link previous and next files
     if prev_filepath:
-        prev = prev_filepath.split("/")[-1]
+        prev = path.basename(prev_filepath)
         xml_current_root.attrib['prev'] = f"https://id.acdh.oeaw.ac.at/ofmgraz/teidocs/{prev}"
-        xml_prev = TeiReader(prev_filepath)
+
+        # Reuse TeiReader from cache if available
+        if prev not in tei_cache:
+            xml_prev = TeiReader(prev_filepath)
+            tei_cache[prev] = xml_prev
+        else:
+            xml_prev = tei_cache[prev]
+        
         xml_prev_root = xml_prev.tree.getroot()
         xml_prev_root.attrib['next'] = f"https://id.acdh.oeaw.ac.at/ofmgraz/teidocs/{filename}"
+        
         with open(prev_filepath, "w") as f:
             f.write(ET.tostring(xml_prev_root, pretty_print=True, encoding="unicode"))
+    
     prev_filepath = tkb_file
+    
     with open(tkb_file, "w") as f:
         f.write(ET.tostring(xml_current_root, pretty_print=True, encoding="unicode"))
--- pyscripts/transform.py
+++ pyscripts/transform.py
@@ -4,25 +4,42 @@ from pathlib import Path
 import glob
 from saxonche import PySaxonProcessor
 
+# Constants
 XSLT = "https://csae8092.github.io/page2tei/page2tei-0.xsl"
 METS_DIR = Path("./data/mets")
 TEI_DIR = Path("./data/editions")
 col_id = "216937"
+
+# Prepare directories and files
 files = glob.glob(f"{METS_DIR}/{col_id}/*_mets.xml")
 os.makedirs(TEI_DIR, exist_ok=True)
 
-for x in files:
-    tail = os.path.split(x)[-1]
-    doc_id = tail.split("_")[0]
-    tei_file = f"{doc_id}.xml"
-    print(f"transforming mets: {x} to {TEI_DIR}/{tei_file}")
-    with PySaxonProcessor(license=False) as proc:
-        xsltproc = proc.new_xslt30_processor()
-        xsltproc.set_parameter("combine", proc.make_boolean_value(True))
-        xsltproc.set_parameter("ab", proc.make_boolean_value(True))
-        document = proc.parse_xml(xml_file_name=x)
-        executable = xsltproc.compile_stylesheet(stylesheet_file=XSLT)
-        output = executable.transform_to_string(xdm_node=document)
-        output = output.replace(' type=""', "")
-        with open(os.path.join(TEI_DIR, tei_file), "w") as f:
-            f.write(output)
+def transform_file(x):
+    try:
+        tail = os.path.split(x)[-1]
+        doc_id = tail.split("_")[0]
+        tei_file = f"{doc_id}.xml"
+        tei_path = os.path.join(TEI_DIR, tei_file)
+        print(f"Transforming METS: {x} to {tei_path}")
+
+        # Initialize the processor and process one file at a time
+        with PySaxonProcessor(license=False) as proc:
+            xsltproc = proc.new_xslt30_processor()
+            xsltproc.set_parameter("combine", proc.make_boolean_value(True))
+            xsltproc.set_parameter("ab", proc.make_boolean_value(True))
+
+            document = proc.parse_xml(xml_file_name=x)
+            executable = xsltproc.compile_stylesheet(stylesheet_file=XSLT)
+            output = executable.transform_to_string(xdm_node=document)
+            output = output.replace(' type=""', "")
+
+            # Write the output to a TEI file
+            with open(tei_path, "w") as f:
+                f.write(output)
+
+    except Exception as e:
+        print(f"Error processing {x}: {e}")
+
+# Sequential Processing
+for file in files:
+    transform_file(file)
