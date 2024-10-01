b742d92cf21356c0bbbf9bc9d7fbd8141ea92115
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-10-01 17:29:01 +0200

    declaration

--- shellscripts/full_workflow.sh
+++ shellscripts/full_workflow.sh
@@ -11,4 +11,4 @@ rm -f data/mets/*/{2914584,2926205,3374447,3374448,3374547,3376967,3377127,33772
 ./pyscripts/generate_body_p.py
 cp -f data/constants/S1*.xml data/editions
 ./pyscripts/add_handles.py data/editions/* |nl
-./add_declaration.sh
+./shellscripts/add_declaration.sh
