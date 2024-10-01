afb56d038ccc84ac074fa1568af1e2864ab8167b
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-10-01 17:31:41 +0200

    dec

--- shellscripts/add_declaration.sh
+++ shellscripts/add_declaration.sh
@@ -5,5 +5,5 @@ else
 	XMLPATH="$1"
 fi
 
-sed -i $'1s/^/<?xml version="1.0" encoding="utf-8"?>\\\n<?xml-model href="https:\\/\\/id.acdh.oeaw.ac.at\\/ofmgraz\\/schema.rng" type="application\\/xml" schematypens="http:\\/\\/relaxng.org\\/ns\\/structure\\/1.0"?>\\\n/' $XMLPATH/*.xml
+sed -i $'1s/^/<?xml version="1.0" encoding="UTF-8"?>\\\n<?xml-model href="https:\\/\\/id.acdh.oeaw.ac.at\\/ofmgraz\\/schema.rng" type="application\\/xml" schematypens="http:\\/\\/relaxng.org\\/ns\\/structure\\/1.0"?>\\\n/' $XMLPATH/*.xml
 
