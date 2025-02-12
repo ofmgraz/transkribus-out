1c60deab44efd88b9bc6721699169bbd07e7c8f0
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2025-02-12 13:55:32 +0100

    find instead of rm directly

--- shellscripts/full_workflow.sh
+++ shellscripts/full_workflow.sh
@@ -2,7 +2,7 @@
 . ./secret.env
 rm -rf data/mets/* data/editions/* data/constants/tei_headers/*
 ./pyscripts/download.py
-rm -f data/mets/*/{2914584,2926205,3374447,3374448,3374547,3376967,3377127,3377287,3385787,3406471,3674355}* ||true
+find data/mets/ -type f \( -name "2914584*" -o -name "2926205*" -o -name "3374447*" -o -name "3374448*" -o -name "3374547*" -o -name "3376967*" -o -name "3377127*" -o -name "3377287*" -o -name "3385787*" -o -name "3406471*" -o -name "3674355*" -o -name "6327169*" -o -name "6370807*" \) -exec rm -f {} +
 ./pyscripts/transform.py |nl
 ./pyscripts/amend_duplicate.py || true
 ./shellscripts/amend_pics_paths.sh || true
